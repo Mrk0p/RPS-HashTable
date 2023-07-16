407165f1c2f7497c7bbf61b68786598eae946f29
Mrk0p <sachinwele23@gamil.com>
2023-07-16 15:53:37 +0530

    Added:remove avoidable word from phrase

--- src/main/java/com/bridgelabz/hashtable/HashTable.java
+++ src/main/java/com/bridgelabz/hashtable/HashTable.java
@@ -4,23 +4,53 @@ import java.util.HashMap;
 import java.util.Set;
 
 public class HashTable {
-    static void inputSentence(String userString) {
+
+    static void userInputSentence(String userString) {
+
         HashMap<String, Integer> frequency = new HashMap<String, Integer>();
-        String[] words = userString.split(" ");
+        String[] words = userString.split(" "); // splitting to find word
         for (String word : words) {
             if (frequency.containsKey(word)) {
                 frequency.put(word, frequency.get(word) + 1);
             } else {
-                frequency.put(word, 1);
+                frequency.put(word, 1); // storing the word as key
             }
         }
         Set<String> stringFrequency = frequency.keySet();
-        System.out.println("Frequency Of Words In Sentence: \n 'To Be Or Not To Be'");
+        System.out.println("Frequency Of Words In Sentence: \n'Paranoids are not paranoid because\n"
+                + "they are paranoid but because they keep putting themselves deliberately into\n"
+                + "paranoid avoidable situations' is : ");
         for (String word : stringFrequency) {
-            System.out.println(word + " = " + frequency.get(word) + " times.");
+            if (frequency.get(word) > 1)
+                System.out.println(word + " = " + frequency.get(word) + " times.");
 
         }
     }
+    public static void remove(String string, String removeWord) {
+        String[] stringArray = string.split(" ");
+        String newSentenceAfterRemovingWord = " ";
+
+
+        for (String words : stringArray) {
+
+            if (!words.equals(removeWord)) {
+
+                newSentenceAfterRemovingWord = newSentenceAfterRemovingWord + words + " ";
+
+            }
+        }
+
+        System.out.println(newSentenceAfterRemovingWord);
+    }
+
     public static void main(String[] args) {
-        inputSentence("To Be Or Not To Be");
-    }}
+        userInputSentence("Paranoids are not paranoid because they are paranoid but because "
+                + "they keep putting themselves deliberately into paranoid avoidable situations");
+
+
+
+        String string = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+        String removeWord = "avoidable";
+        remove(string, removeWord);
+    }
+}
