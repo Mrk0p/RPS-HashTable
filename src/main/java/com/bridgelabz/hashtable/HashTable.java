3f2d7156e7ddef1c9b1381a6c3662e1845b4d1ed
Mrk0p <sachinwele23@gamil.com>
2023-07-16 15:47:58 +0530

    Added:Welcome message of hash table program

---
+++ src/main/java/com/bridgelabz/hashtable/HashTable.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.hashtable;
+
+public class HashTable {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to hash table program.");
+    }
+}
