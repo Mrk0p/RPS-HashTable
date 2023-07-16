0df90b081cc109acc1dd8a89d0e5a4810eceb733
Mrk0p <sachinwele23@gamil.com>
2023-07-16 15:50:13 +0530

    Added:frequency of words in paragraph

--- src/main/java/com/bridgelabz/hashtable/HashTable.java
+++ src/main/java/com/bridgelabz/hashtable/HashTable.java
@@ -1,8 +1,31 @@
 package com.bridgelabz.hashtable;
 
-public class HashTable {
-    public static void main(String[] args) {
+import java.util.HashMap;
+import java.util.Set;
 
-        System.out.println("Welcome to hash table program.");
+public class HashTable {
+    static void inputSentence(String userString) {
+        HashMap<String, Integer> frequency = new HashMap<String, Integer>();
+        String[] words = userString.split(" "); // splitting to find word
+        for (String word : words) {
+            if (frequency.containsKey(word)) {
+                frequency.put(word, frequency.get(word) + 1);
+            } else {
+                frequency.put(word, 1); // storing the word as key
+            }
+        }
+        Set<String> stringFrequency = frequency.keySet();
+        System.out.println("Frequency Of Words In Sentence: \n'Paranoids are not paranoid because\n"
+                + "they are paranoid but because they keep putting themselves deliberately into\n"
+                + "paranoid avoidable situations' is : ");
+        for (String word : stringFrequency) {
+            if (frequency.get(word) > 1)
+                System.out.println(word + " = " + frequency.get(word) + " times.");
+
+        }
+    }
+    public static void main(String[] args) {
+        inputSentence("Paranoids are not paranoid because they are paranoid but because "
+                + "they keep putting themselves deliberately into paranoid avoidable situations");
     }
 }
